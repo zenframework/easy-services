ddb2503c2f38d3d4734df76c48eeea3bb697e7e0
Oleg S. Lekshin <oleg.lekshin@gmail.com>
2017-02-14 21:57:12 +0300

    Abstract caching service request

---
+++ easy-services-core/src/main/java/org/zenframework/easyservices/impl/AbstractServiceRequest.java
@@ -0,0 +1,37 @@
+package org.zenframework.easyservices.impl;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.zenframework.easyservices.ServiceRequest;
+
+public abstract class AbstractServiceRequest implements ServiceRequest {
+
+    private byte[] cachedData;
+
+    @Override
+    public void cacheInput() throws IOException {
+        InputStream in = internalGetInputStream();
+        try {
+            byte[] buf = new byte[8192];
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            for (int n = in.read(buf); n >= 0; n = in.read(buf))
+                out.write(buf, 0, n);
+            cachedData = out.toByteArray();
+        } finally {
+            in.close();
+        }
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        if (cachedData != null)
+            new ByteArrayInputStream(cachedData);
+        return internalGetInputStream();
+    }
+
+    abstract protected InputStream internalGetInputStream() throws IOException;
+
+}
