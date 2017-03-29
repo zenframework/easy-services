646ab07b78101845a22e7fdcccee244a0225a6c0
Oleg S. Lekshin <oleg.lekshin@gmail.com>
2017-03-29 18:33:04 +0300

    Simple filter streams

---
+++ easy-services-util/src/main/java/org/zenframework/easyservices/util/io/FilterOutputStream.java
@@ -0,0 +1,39 @@
+package org.zenframework.easyservices.util.io;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class FilterOutputStream extends OutputStream {
+
+    protected final OutputStream out;
+
+    public FilterOutputStream(OutputStream out) {
+        this.out = out;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        out.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        out.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        out.write(b, off, len);
+    }
+
+    @Override
+    public void flush() throws IOException {
+        out.flush();
+    }
+
+    @Override
+    public void close() throws IOException {
+        out.close();
+    }
+
+}
